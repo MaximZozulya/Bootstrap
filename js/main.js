820a5949ff9d0c50c1a8cf857f74608494043a7f
Maxim <soundmax15041987@gmail.com>
2024-12-03 13:29:24 +0300

    Implemented a scroll up button

--- js/main.js
+++ js/main.js
@@ -1,3 +1,20 @@
 $(function(){
   baguetteBox.run('.gallery');
+
+  //Показать , скрыть кнопку вверх
+  $(window).scroll(function(){
+    if ($(this).scrollTop() > 300) {
+      $('.scrollToTop').fadeIn();
+    }
+    else {
+      $('.scrollToTop').fadeOut();
+    }
+  });
+
+  // Анимация прокрутки кнопкки вверх
+  $('.scrollToTop').click(function(){
+    $('html, body').animate({scrollTop : 0}, 800);
+    return false;
+  });
+
 });
